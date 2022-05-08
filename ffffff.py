5647154bee5ca1ab7f7f86e8c82a8648e004d610
Dhia_Oussayed <75687069+Dhiaeddine-Oussayed@users.noreply.github.com>
2022-05-08 11:51:41 +0100

    Update ffffff.py

--- ffffff.py
+++ ffffff.py
@@ -37,7 +37,6 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
 
 le = preprocessing.LabelEncoder()
 le.fit(y)
-le.classes_
 y = le.transform(y)
 Y = to_categorical(y, num_classes=4)
 
